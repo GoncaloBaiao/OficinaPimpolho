fff25e340142e54f1d5682441e597209468a77ef
RexCaelum <aluno22113@ipt.pt>
2023-07-11 13:17:10 +0100

    view Marcações

--- Models/Marcacao.cs
+++ Models/Marcacao.cs
@@ -18,7 +18,7 @@ namespace OficinaPimpolho.Models
         [StringLength(50, ErrorMessage = "O nome não pode conter mais que 50 letras.\n Se for necessário abrevie o nome.")]
         [Column("Nome")]
         [Display(Name = "Nome")]
-        public string Nome { get; set; }
+        public string Nome { get; set; } 
 
         [Required]
         public double Preco { get; set; }
